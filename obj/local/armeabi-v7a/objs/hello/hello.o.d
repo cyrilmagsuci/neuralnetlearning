2da78f7499463b72ae6a873ccf0fc37f471ef2f9
Michael Cyril Magsuci <cyrilmagsuci@live.com>
2016-01-22 09:37:32 +0800

    neural net algo Steven C. Shaffer

---
+++ obj/local/armeabi-v7a/objs/hello/hello.o.d
@@ -0,0 +1,19 @@
+obj/local/armeabi-v7a/objs/hello/hello.o: jni/hello.cpp \
+ /data/data/com.aide.ui/files/ndksupport-20150805/android-ndk-aide/sources/cxx-stl/gnu-libstdc++/4.9/include/iostream \
+ /data/data/com.aide.ui/files/ndksupport-20150805/android-ndk-aide/platforms/android-3/arch-arm/usr/include/stdlib.h \
+ /data/data/com.aide.ui/files/ndksupport-20150805/android-ndk-aide/platforms/android-3/arch-arm/usr/include/alloca.h \
+ /data/data/com.aide.ui/files/ndksupport-20150805/android-ndk-aide/platforms/android-3/arch-arm/usr/include/strings.h \
+ /data/data/com.aide.ui/files/ndksupport-20150805/android-ndk-aide/platforms/android-3/arch-arm/usr/include/memory.h \
+ /data/data/com.aide.ui/files/ndksupport-20150805/android-ndk-aide/sources/cxx-stl/gnu-libstdc++/4.9/include/cmath
+
+/data/data/com.aide.ui/files/ndksupport-20150805/android-ndk-aide/sources/cxx-stl/gnu-libstdc++/4.9/include/iostream:
+
+/data/data/com.aide.ui/files/ndksupport-20150805/android-ndk-aide/platforms/android-3/arch-arm/usr/include/stdlib.h:
+
+/data/data/com.aide.ui/files/ndksupport-20150805/android-ndk-aide/platforms/android-3/arch-arm/usr/include/alloca.h:
+
+/data/data/com.aide.ui/files/ndksupport-20150805/android-ndk-aide/platforms/android-3/arch-arm/usr/include/strings.h:
+
+/data/data/com.aide.ui/files/ndksupport-20150805/android-ndk-aide/platforms/android-3/arch-arm/usr/include/memory.h:
+
+/data/data/com.aide.ui/files/ndksupport-20150805/android-ndk-aide/sources/cxx-stl/gnu-libstdc++/4.9/include/cmath:
